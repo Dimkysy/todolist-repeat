65341a9b7e51f27f56eeb9b6302618501d120562
Dimkysy <diman.nesterov1995@gmail.com>
2022-06-01 21:54:03 +0300

    finish first lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ import './App.css';
 import Todolist from './Todolist';
 
 function App() {
+
+    const tasks1 = [
+        {id:1, title:"HTML & CSS", isDone:true},
+        {id:2, title:"JS", isDone:false},
+        {id:3, title:"Reactjs", isDone:true},
+    ]
+
+    const tasks2 = [
+        {id:1, title:"Hello world", isDone:true},
+        {id:2, title:"Happy hacking ", isDone:false},
+        {id:3, title:"Yo", isDone:true},
+    ]
+
     return (
         <div className="App">
-           <Todolist/>
+           <Todolist title ="What the title" tasks={tasks1}  />
+           <Todolist title ="I title" tasks={tasks2} />
         </div>
     );
 }
