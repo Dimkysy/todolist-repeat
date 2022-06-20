da25f3401feba56803fc86b5a30d7fc7bb29c0b5
Dimkysy <diman.nesterov1995@gmail.com>
2022-06-20 13:51:00 +0300

    all zero

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import Todolist from './Todolist';
-import {v1} from "uuid";
-
-export type FilterValuesType = "all" | "active" | "completed"
 
 function App() {
-
-    let [tasks, setTasks] = useState([
-        {id:v1(), title:"HTML & CSS", isDone:true},
-        {id:v1(), title:"JS", isDone:false},
-        {id:v1(), title:"Reactjs", isDone:true},
-    ])
-
-    function removeTask(id:string) {
-        let fileteredTasks = tasks.filter(t => t.id != id)
-        setTasks(fileteredTasks)
-    }
-
-    let [filter, setFilter] = useState<FilterValuesType>("all");
-
-    let tasksForTodolist = tasks
-
-    if (filter === "active") {
-        tasksForTodolist = tasks.filter(t => t.isDone === false)
-    }
-    if (filter === "completed") {
-        tasksForTodolist = tasks.filter(t => t.isDone === true)
-    }
-
-    function changeFilter(value:FilterValuesType) {
-        setFilter(value)
-    }
-
-    function addTask(title:string) {
-        let task = {id:v1(), title:title, isDone:false}
-        let newTasks = [task, ...tasks]
-        setTasks(newTasks)
-    }
-
     return (
         <div className="App">
-           <Todolist
-               title ="What the title"
-               tasks={tasksForTodolist}
-               removeTask={removeTask}
-               changeFilter = {changeFilter}
-               addTask = {addTask}
-           />
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
         </div>
     );
 }
