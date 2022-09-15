348d5c50f103872536e610ed64e7941c36278a55
Dimkysy <diman.nesterov1995@gmail.com>
2022-09-15 09:38:17 +0300

    repeat6 lesson1

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
+
 
 function App() {
+
+    const tasks1 = [
+        {id:1, isDone:false, title:"React"},
+        {id:2, isDone: true, title: "Angular"},
+        {id:3, isDone: false, title: "Vue"}
+    ]
+
+    const tasks2 = [
+        {id:1, isDone:true, title:"JS"},
+        {id:2, isDone: false, title: "CSS"},
+        {id:3, isDone: true, title: "HTML"}
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist
+                title ="Hello world"
+                tasks = {tasks1}
+
+            />
+            <Todolist
+                title ="Bear"
+                tasks = {tasks2}
+            />
         </div>
     );
 }
 
-export default App;
+    export default App;
