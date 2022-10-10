aac22400f392f6c4e4c432077eb75dfc6ab5e337
Dimkysy <diman.nesterov1995@gmail.com>
2022-10-10 13:42:42 +0300

    repeat7 lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import './App.css';
+import Tododlist from "./Todolist";
 
 function App() {
+
+    let task1 = [
+        {id:1, title:"HTML&CSS", isDone:true},
+        {id:2, title:"Js", isDone:true},
+        {id:3, title:"React", isDone:false}
+    ];
+
+    let task2 = [
+        {id:1, title:"Redux", isDone:true},
+        {id:2, title:"ReduxTullCit", isDone:false},
+        {id:3, title:"Saga", isDone:true}
+    ]
+
+
     return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+        <div>
+             <Tododlist title="JS"  tasks={task1}/>
+            <Tododlist title="React" tasks={task2}/>
         </div>
     );
 }
+
+
+
+
 
 export default App;
