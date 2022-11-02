b40cec96a42501e707d0516e12fc8fc646e419f7
Dimkysy <diman.nesterov1995@gmail.com>
2022-11-02 15:30:00 +0300

    repeat1 lesson 4

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import {filterValuesType, TaskType} from "./App";
 import './App.css';
 
@@ -12,22 +12,51 @@ type PropsType = {
 
 
 export  function Todolist(props:PropsType) {
+
+
+    let [title, setTitle] = useState("")
+
+    let addTask = () => {
+        props.addTasks(title)
+        setTitle("")
+    }
+
+    let onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    // @ts-ignore
+    let onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) => {
+        if (e.charCode === 13) {
+            addTask()
+        }
+    }
+
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button onClick={() => props.addTasks("Hi")} >+</button>
+                <input
+                    value = {title}
+                    onChange={onChangeHandler}
+                    onKeyPress={onKeyPressHandler}
+                />
+                <button onClick={addTask} >+</button>
             </div>
             <ul>
                 {
-                    props.tasks.map(t => <li key={t.id}>
-                            <input type="checkbox" checked={t.isDone}/>
-                            <span>{t.title}</span>
-                            <button className="removeButton"  onClick={()=> props.removeTasks(t.id)} >x</button>
-                    </li>
-                    )
+                    props.tasks.map(t => {
+                        let onClickHandler = () => props.removeTasks(t.id)
+                        return (
+                            <li key={t.id}>
+                                <input type="checkbox" checked={t.isDone}/>
+                                <span>{t.title}</span>
+                                <button className="removeButton"  onClick={onClickHandler} >x</button>
+                            </li>
+                        )
+                    })
                 }
+
             </ul>
             <div>
                 <button onClick={()=> props.changeFilter("all")} >All</button>
